4367360c0ca589395d11b6766fad2719cc0b00fb
RaviCharan2929 <86793390+RaviCharan2929@users.noreply.github.com>
2023-10-17 15:29:06 +0530

    first commit

---
+++ src/WordCounter.js
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import Navbar from "./components/Navbar";
+import TextBox from "./components/TextBox";
+import Alert from "./components/Alert";
+
+function WordCounter() {
+  const [mode, setMode] = useState("light");
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (text, type) => {
+    setAlert({
+      text: text,
+      type: type,
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  };
+
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "black";
+      showAlert("Dark Mode Enabled", "success");
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+      showAlert("Light Mode Enabled", "success");
+    }
+  };
+
+  return (
+    <>
+      <Navbar
+        title="Text Utils"
+        about="I am nothing"
+        mode={mode}
+        toggleMode={toggleMode}
+      />
+      <Alert alert={alert} />
+      <TextBox action="To Uppercase" mode={mode} showAlert={showAlert} />
+    </>
+  );
+}
+
+export default WordCounter;
